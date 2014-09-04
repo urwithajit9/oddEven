fa6514afbb37edaefb3281001d572380252aad29
Ajit kumar <urwithajit9@gmail.com>
2014-09-04 22:14:15 +0530

    First commit

---
+++ oddEven.py
@@ -0,0 +1,19 @@
+"""
+This is program for finding a given number is odd or even.
+This is written just for testing purpose.
+
+Develop by: Ajit kumar ( github/urwithajit)
+Date: 4Sept2014
+"""
+number = int(raw_input("Please a nubmer to check Odd or Even :"))
+
+def check_odd_even(num):
+	if num % 2 == 0:
+		return "Even"
+
+result = check_odd_even(number)
+
+if result == "Even":
+	print("The given number is a Even number.")
+
+
